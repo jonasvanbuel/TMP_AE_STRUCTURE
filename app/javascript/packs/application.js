0c86fda4943393109914427941173103e9d8cad9
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-05-13 09:14:02 +0100

    added service workers PWA functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,3 +28,25 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// Register service worker
+// Source: https://dev.to/coorasse/the-progressive-rails-app-46ma
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    console.log('ServiceWorker registered: ', registration);
+
+    var serviceWorker;
+    if (registration.installing) {
+      serviceWorker = registration.installing;
+      console.log('Service worker installing.');
+    } else if (registration.waiting) {
+      serviceWorker = registration.waiting;
+      console.log('Service worker installed & waiting.');
+    } else if (registration.active) {
+      serviceWorker = registration.active;
+      console.log('Service worker active.');
+    }
+  }).catch(registrationError => {
+    console.log('Service worker registration failed: ', registrationError);
+  });
+});
