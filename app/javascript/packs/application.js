a55bf6eafa47d57d03d70933fed75bd31a1acad4
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-05-13 11:44:41 +0100

    service worker light

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,25 +30,26 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 
-
-
-// Register service worker
+// Load service worker
 // Source: https://dev.to/coorasse/the-progressive-rails-app-46ma
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('/service-worker.js').then(registration => {
-    console.log('ServiceWorker registered: ', registration);
+// Source: https://www.youtube.com/watch?v=c9slVBXsXyo&ab_channel=MikeRogers
 
-    var serviceWorker;
-    if (registration.installing) {
-      serviceWorker = registration.installing;
-      console.log('Service worker installing.');
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-      console.log('Service worker installed & waiting.');
-    } else if (registration.active) {
-      serviceWorker = registration.active;
-      console.log('Service worker active.');
-    }
+window.addEventListener('load', () => {
+  // Update filename service worker below
+  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+
+    // console.log('ServiceWorker registered: ', registration);
+    // var serviceWorker;
+    // if (registration.installing) {
+    //   serviceWorker = registration.installing;
+    //   console.log('Service worker installing.');
+    // } else if (registration.waiting) {
+    //   serviceWorker = registration.waiting;
+    //   console.log('Service worker installed & waiting.');
+    // } else if (registration.active) {
+    //   serviceWorker = registration.active;
+    //   console.log('Service worker active.');
+    // }
   }).catch(registrationError => {
     console.log('Service worker registration failed: ', registrationError);
   });
--- app/javascript/service_workers/service-worker.js
+++ app/javascript/service_workers/service-worker.js
@@ -1,10 +1,12 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker installing.');
-});
+// self.addEventListener('install', function(event) {
+//     console.log('Service Worker installing.');
+// });
 
-self.addEventListener('activate', function(event) {
-    console.log('Service Worker activated.');
-});
-self.addEventListener('fetch', function(event) {
-    console.log('Service Worker fetching.');
-});
+// self.addEventListener('activate', function(event) {
+//     console.log('Service Worker activated.');
+// });
+// self.addEventListener('fetch', function(event) {
+//     console.log('Service Worker fetching.');
+// });
+
+console.log('Im inside the service-worker...');
